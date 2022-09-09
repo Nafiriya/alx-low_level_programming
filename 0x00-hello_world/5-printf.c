4bc6b376bb2525813efa025ac76939d8f227438b
Nafiriya <nafimom@gmail.com>
2022-09-09 05:13:52 -0700

    program that prints exactly with proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,7 @@
 /**
 * main - prints a text
 *
-* Discription: using the main function
-* This program prints'rogramming is like building a multilingual puzzle'
-* return: 0
+* return: 0 
 */
 int main(void)
 {
