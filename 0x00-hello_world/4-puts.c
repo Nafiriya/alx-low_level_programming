7c86174229d988a72b607e734d9f2a7759e88646
Nafiriya <nafimom@gmail.com>
2022-09-09 01:43:17 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
 * Discription: using the main function
 * This programm prints "Programming is like building a multilingual puzzle"
 * Return: 0 
-*/
-int main ()
+* */
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
