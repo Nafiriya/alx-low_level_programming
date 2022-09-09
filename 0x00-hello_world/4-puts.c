311f8590820171a097660cd94334d0f0ea42d8f0
Nafiriya <nafimom@gmail.com>
2022-09-09 04:04:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
-* main - printing the string in the put function
-*
-* Discription: using the main function
-* This programm prints "Programming is like building a multilingual puzzle"
-* Return: 0 
-* */
-int main (void)
+ * Discription: using the main function
+ * Return: 0 if success.
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");                   return (0);
 }
