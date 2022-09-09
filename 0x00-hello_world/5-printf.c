f39c0d47abd5bffffdf516b3112f27b1a5c1105c
Nafiriya <nafimom@gmail.com>
2022-09-09 05:20:47 -0700

    program that prints exactly with proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-/**
-* main - prints a text
+
+**/
+* main - Print a text
 *
-* return: 0 
+* Discription: using the main function
+* This program prints'rogramming is like building a multilingual puzzle'
+* return: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+      printf("with proper grammar, but the outcome is a piece of art,\n");
+      return (0);
 }
